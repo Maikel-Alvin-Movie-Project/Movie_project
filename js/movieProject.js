cc35fea553d09208ae30a86f2b00c69e2b51bbc0
Maikel Karam <maikel.m.karam@gmail.com>
2022-11-02 16:59:27 -0500

    Displayed movie info in HTML

--- js/movieProject.js
+++ js/movieProject.js
@@ -1,5 +1,18 @@
 //https://silk-admitted-crow.glitch.me/movies
 
+let movieTitle;
+
 fetch('https://silk-admitted-crow.glitch.me/movies')
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then( data => {
+        console.log(data);
+        displayMovies(data);
+    });
+function displayMovies(data){
+        let mainContainer = document.getElementById("movie");
+        for (let i = 0; i < data.length; i++){
+                let div = document.createElement("div");
+                div.innerHTML = 'Title: ' + data[i].title + ' Rating: ' + data[i].rating;
+                mainContainer.appendChild(div);
+        }
+}
