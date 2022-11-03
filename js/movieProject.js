e6a2742e98e221707f8cca85d737a544cd24b567
Maikel Karam <maikel.m.karam@gmail.com>
2022-11-03 12:05:03 -0500

    Loader Progress Save

--- js/movieProject.js
+++ js/movieProject.js
@@ -2,7 +2,15 @@
 
 
 //load Screen Start
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
 
+    loader.classList.add("loader-hidden");
+
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild("loader");
+    })
+})
 
 
 
